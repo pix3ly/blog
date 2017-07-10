9fed0bf3f0ea6509978e0cf4bee0e09a52f712b4
Daniël van Drunen <daniel@pixely.me>
2017-07-10 18:25:04 +0300

    Add controller for posts

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -2,7 +2,9 @@
 <ul>
     @foreach ($blog->posts as $post)
         <li>
-            <h2>{{ $post->title }}</h2>
+            <h2>
+                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+            </h2>
         </li>
     @endforeach
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/blogs', 'BlogsController');
+Route::resource('/posts', 'PostsController');
