5172183f1e084e15ea3e5fc95fa49e8b9652fc3d
Daniël van Drunen <daniel@pixely.me>
2017-07-10 16:00:48 +0300

    Add relation to posts in blogs' model

--- app/Blog.php
+++ app/Blog.php
@@ -5,5 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model {
-    //
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 }
--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,1 +1,8 @@
 <h1>{{ $blog->title }}</h1>
+<ul>
+    @foreach ($blog->posts as $post)
+        <li>
+            <h2>{{ $post->title }}</h2>
+        </li>
+    @endforeach
+</ul>
