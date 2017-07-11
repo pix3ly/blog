cd0616fc2f402c637e21ab50f21143f97d2eff7b
Daniël van Drunen <daniel@pixely.me>
2017-07-12 02:34:11 +0300

    Change position/styling of rating in posts' show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,18 @@
 
 @section('body')
     <a class="post__back" href="/blogs/{{ $post->blog->id }}">Go back to '{{ $post->blog->title }}'</a>
-    <h1 class="post__title">{{ $post->title }}</h1>
+    <div class="row">
+        <div class="row__column row__column--align-center row__column--align-middle">
+            <div class="post__rating">
+                <a href="#">+</a>
+                <h2 class="rating__net">{{ $post->netVotes() }}</h2>
+                <a href="#">-</a>
+            </div>
+        </div>
+        <div class="row__column">
+            <h1 class="post__title">{{ $post->title }}</h1>
+        </div>
+    </div>
     <div class="post__body">{!! $post->body !!}</div>
-    <p class="post__date">{{ date('F jS, Y', strtotime($post->date)) }} &middot; {{ $post->netVotes() }} rating</p>
+    <p class="post__date">{{ date('F jS, Y', strtotime($post->date)) }}</p>
 @endsection
