f9d205d0d557602ec26366234c55eeb710cd62ad
Daniël van Drunen <daniel@pixely.me>
2017-07-12 05:35:54 +0300

    Add store function to LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\User;
 
 class LoginController extends Controller {
     public function index() {
         return view('login.index');
+    }
+
+    public function store(Request $request) {
+        $email = $request->input('email');
+
+        if ($user = User::where('email', $email)->first()) {
+            $password = $request->input('password');
+
+            if (Hash::check($password, $user->password)) {
+                return redirect('/blogs/1');
+            }
+
+            return redirect('/login');
+        }
+
+        return redirect('/login');
     }
 }
