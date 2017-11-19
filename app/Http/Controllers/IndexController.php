b73cc0646dcd22f80c7e5291ffa7c525022e27ee
Daniël van Drunen <daniel@pixely.me>
2017-11-19 19:50:25 +0100

    Order posts by date DESC on index view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,7 @@ use App\Post;
 
 class IndexController extends Controller {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::orderBy('posted_on', 'DESC')->get();
 
         return view('index', [
             'posts' => $posts
