12898d013f65b45b98c1cf5209ad0b8e2880118f
Daniël van Drunen <daniel@pixely.me>
2017-11-19 16:54:28 +0100

    Add Tag model and migrations for it's many-to-many relationship to posts

--- app/Post.php
+++ app/Post.php
@@ -5,9 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use App\User;
+use App\Tag;
 
 class Post extends Model {
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,9 @@
     <p>By {{ $post->user->name }}</p>
     <p>{{ date('F jS, Y', strtotime($post->posted_on)) }}</p>
     <div>{!! $post->body !!}</div>
+    <ul>
+        @foreach ($post->tags as $tag)
+            <li>{{ $tag->name }}</li>
+        @endforeach
+    </ul>
 @endsection
