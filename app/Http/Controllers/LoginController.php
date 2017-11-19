afb2a07b02fc99500c3bf9c107ae1a3ae517dd60
Daniël van Drunen <daniel@pixely.me>
2017-11-19 15:43:28 +0100

    Add LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class LoginController extends Controller {
+    public function index() {
+        return view('login');
+    }
+
+    public function store(Request $request) {
+        if (Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ])) {
+            return redirect()->route('index_get');
+        }
+
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 
 Route::get('/', 'IndexController@index')->name('index_get');
 Route::get('/posts/{id}', 'PostsController@show')->name('posts_get');
+
+Route::get('/login', 'LoginController@index')->name('login');
+Route::post('/login', 'LoginController@store');
