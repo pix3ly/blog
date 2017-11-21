e303cd4f186849f00edcfbb695f8d559effcaf21
Daniël van Drunen <daniel@pixely.me>
2017-11-21 17:24:26 +0100

    Add navigation styling and move from Libre Baskerville font to Muli

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,17 +2,21 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:700,700i" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:400,400i" />
         <link rel="stylesheet" href="/css/app.css" />
     </head>
     <body>
         <div id="app">
             @if (Auth::check())
-                <ul>
-                    <li>
-                        <a href="/logout">Log out</a>
-                    </li>
-                </ul>
+                <div class="navigation">
+                    <div class="wrapper">
+                        <ul>
+                            <li>
+                                <a href="/logout">Log out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             @endif
             <div class="wrapper">
                 @yield('body')
