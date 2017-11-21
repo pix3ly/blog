3f273b64ffacb307894b36025c27c63bc687369b
Daniël van Drunen <daniel@pixely.me>
2017-11-21 19:15:10 +0100

    Stop displaying seconds at timestamps

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <h1 class="spacing-bottom-medium text-align-center">{{ $post->title }}</h1>
-    <div class="text-align-center">{{ date('F jS, Y H:i:s', strtotime($post->posted_on)) }} &middot; By {{ $post->user->name }}</div>
+    <div class="text-align-center">{{ date('F jS, Y H:i', strtotime($post->posted_on)) }} &middot; By {{ $post->user->name }}</div>
     <div class="seperator"></div>
     <div class="body">{!! $post->body !!}</div>
 @endsection
