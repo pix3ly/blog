22bc8236878692ab1135fd6659ecbe2c44bfc16a
Daniël van Drunen <daniel@pixely.me>
2017-11-21 17:13:12 +0100

    Display time that post was posted on posts' show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <h1 class="spacing-bottom-small">{{ $post->title }}</h1>
-    <div>{{ date('F jS, Y', strtotime($post->posted_on)) }} &middot; By {{ $post->user->name }}</div>
+    <div>{{ date('F jS, Y H:i:s', strtotime($post->posted_on)) }} &middot; By {{ $post->user->name }}</div>
     <div class="seperator"></div>
     <div class="body">{!! $post->body !!}</div>
 @endsection
