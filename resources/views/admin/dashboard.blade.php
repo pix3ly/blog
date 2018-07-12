4b08e53f8b9f0eb959ab55235ddd8777eabaaf21
Daniël van Drunen <daniel@pixely.me>
2018-07-12 20:31:38 +0200

    Style dashboard view in admin area

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,21 +2,28 @@
 
 @section('content')
     <div class="wrapper">
-        <a href="/admin/posts/create">Create</a>
-        <ul>
-            @foreach ($posts as $post)
-                <li class="row row--divide">
-                    <div class="row__column row__column--compact">
-                        <div>{{ $post->title }}</div>
-                        <ul class="tags">
-                            @foreach ($post->tags()->pluck('name')->all() as $tag)
-                                <li>{{ $tag }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    <div class="row__column row__column--compact">{{ $post->posted_on }}</div>
-                </li>
-            @endforeach
-        </ul>
+        <div class="card">
+            <div class="card__header row row--divide">
+                <div class="row__column row__column--compact">Posts</div>
+                <div class="row__column row__column--compact">
+                    <a href="/admin/posts/create">Create</a>
+                </div>
+            </div>
+            <ul class="list list--flush">
+                @foreach ($posts as $post)
+                    <li class="row row--divide">
+                        <div class="row__column row__column--compact">
+                            <div>{{ $post->title }}</div>
+                            <ul class="tags mt-1">
+                                @foreach ($post->tags()->pluck('name')->all() as $tag)
+                                    <li>{{ $tag }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="row__column row__column--compact">{{ $post->posted_on }}</div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
