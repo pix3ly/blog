0ea2101ad069ff1a7530f63233a1b03c394f2737
Daniël van Drunen <daniel@pixely.me>
2018-07-19 14:26:10 +0200

    Add ability to go back to overview and delete tags while editing them

--- resources/views/admin/tags/edit.blade.php
+++ resources/views/admin/tags/edit.blade.php
@@ -3,6 +3,18 @@
 @section('title', 'Admin - Edit Tag')
 
 @section('content')
+    <div class="row row--divide mb-2">
+        <div class="row__column row__column--compact">
+            <a class="btn btn--gray" href="/admin/tags">Go back</a>
+        </div>
+        <div class="row__column row__column--compact">
+            <form method="POST" action="/admin/tags/{{ $tag->id }}">
+                {{ method_field('DELETE') }}
+                {{ csrf_field() }}
+                <button class="btn btn--red">Delete</button>
+            </form>
+        </div>
+    </div>
     <form method="POST" action="/admin/tags/{{ $tag->id }}">
         {{ method_field('PUT') }}
         {{ csrf_field() }}
